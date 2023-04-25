78f8e4f9fe89b2ae08efc60cd691d1cde08bda2f
Stephen Wong <wongstephenky@gmail.com>
2023-04-25 15:09:33 -0700

    updating hash

--- utils/HashMap.py
+++ utils/HashMap.py
@@ -1,2 +1,14 @@
 class HashMap:
-    def __init__(self, length)
+    def __init__(self,size,length, key, value):
+        self.length = length
+        self.key = key
+        self.value = value
+        self.data = []*size
+        
+    def __hash(self, key):
+        hashed = key + 1
+        return hashed
+         
+
+    def __str__(self):
+        return f"{self.key} {self.value}"
